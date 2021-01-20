ab26e5d4252fdb5b7ca951e941e55b1b8347429c
Tatiana Grishechkina <tatiana.grishechkina@amdocs.com>
2021-01-21 02:02:32 +0300

    Updated ex4 - delete first_num

--- ex4.py
+++ ex4.py
@@ -3,12 +3,12 @@ max_num = 0
 
 while number > 10:
     second_num = number % 10
-    first_num = number // 10
     if second_num > max_num:
         max_num = second_num
-    elif 10 > first_num > max_num:
-        max_num = first_num
     number = number // 10
-    print(max_num, first_num, second_num, number)
+    print(max_num, second_num, number)
+
+if number > max_num:
+    max_num = number
 
 print(f'Наибольшая цифра - {max_num}')
